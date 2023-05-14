e8e675ead7f00576d536bbfa30e6c8fca7ea5f5a
alihasan13 <ali.hasan@northsouth.edu>
2023-05-14 23:40:27 +0600

    routes added

---
+++ src/Routes/index.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+import HomeLayout from "../layouts/HomeLayout";
+import ProfileLayout from "../layouts/ProfileLayout";
+import ConnectionLayout from "../layouts/ConnectionLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+ 
+]);
